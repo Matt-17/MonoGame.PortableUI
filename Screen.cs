8f403004af18ef2934b67b8febcfbf592a2a1aac
Matthias Voigt <m.voigt@code-ix.de>
2016-04-12 18:30:44 +0200

    Screen inheritance removed

--- Screen.cs
+++ Screen.cs
@@ -1,26 +1,32 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MonoGame.PortableUI.Controls;
 
 namespace MonoGame.PortableUI
 {
-    public class Screen : Control, IScreen
+    public class Screen : IScreen
     {
-        public Screen(Game game) : base(game)
+        public Screen()
         {
             BackgroundColor = Color.Transparent;
         }
+
+        public Color BackgroundColor { get; set; }
 
         internal ScreenEngine ScreenEngine { get; set; }
 
         public Control Content { get; set; }
 
-        protected internal override void OnDraw(SpriteBatch spriteBatch, Rectangle rect)
+        protected internal void Draw(SpriteBatch spriteBatch)
         {
-            base.OnDraw(spriteBatch, rect);
-
             if (BackgroundColor != Color.Transparent)
                 spriteBatch.GraphicsDevice.Clear(BackgroundColor);
+        }
+
+        public void Update(TimeSpan elapsed)
+        {
+            
         }
     }
 
