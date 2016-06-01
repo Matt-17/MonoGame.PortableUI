54572dad3f18685945df7420bbbdde7ea7d05dbb
Matthias Voigt <matthias.voigt@appsfactory.de>
2016-06-01 15:47:19 +0200

    TextControl not updating on text fixed

--- MonoGame.PortableUI/Controls/TextControl.cs
+++ MonoGame.PortableUI/Controls/TextControl.cs
@@ -6,11 +6,39 @@ namespace MonoGame.PortableUI.Controls
     public abstract class TextControl : Control
     {
         protected SpriteFont Font;
+        private string _text;
+        private int _textSize;
+        private Color _textColor;
 
-        public Color TextColor { get; set; }
+        public Color TextColor
+        {
+            get { return _textColor; }
+            set
+            {
+                _textColor = value;
+                InvalidateLayout(false);
+            }
 
-        public string Text { get; set; }
+        }
 
-        public int TextSize { get; set; }
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                _text = value;
+                InvalidateLayout(true);
+            }
+        }
+
+        public int TextSize
+        {
+            get { return _textSize; }
+            set
+            {
+                _textSize = value;
+                InvalidateLayout(true);
+            }
+        }
     }
 }
