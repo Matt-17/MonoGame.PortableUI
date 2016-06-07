7620fbbdded13150e135728c3a9681bb5a51ed2d
marcus.katscher <marcus.katscher@appsfactory.de>
2016-06-07 10:45:16 +0200

    introducing sample client for ios fix #8

---
+++ SampleClient.iOS/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Foundation;
+using SampleApp;
+using UIKit;
+
+namespace SampleClient.iOS
+{
+    [Register("AppDelegate")]
+    class Program : UIApplicationDelegate
+    {
+        private static SampleGame g;
+
+        internal static void RunGame()
+        {
+            g = new SampleGame();
+            g.Window.AllowUserResizing = true;
+            g.Initialize();
+            g.Window.ClientSizeChanged += (object sender, EventArgs e) =>
+            {
+                g.SetClientSize(g.Window.ClientBounds.Width, g.Window.ClientBounds.Height);
+            };
+            g.Run();
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+
+        public override void FinishedLaunching(UIApplication app)
+        {
+            RunGame();
+        }
+    }
+}
