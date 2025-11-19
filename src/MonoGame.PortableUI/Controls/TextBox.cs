154d39ddcc3d34bc13735965ca5ae68498d67806
Matthias Voigt <m.voigt@code-ix.de>
2025-11-19 16:32:46 +0200

    Fix textbox hint and cursor (fixes #26, fixes #30)

--- src/MonoGame.PortableUI/Controls/TextBox.cs
+++ src/MonoGame.PortableUI/Controls/TextBox.cs
@@ -9,6 +9,8 @@ namespace MonoGame.PortableUI.Controls
 {
     public class TextBox : TextBlock
     {
+        private IKeyboard _attachedKeyboard;
+
         public int CursorPosition { get; set; }
 
         public Brush CursorColor { get; set; }
@@ -43,28 +45,38 @@ namespace MonoGame.PortableUI.Controls
             CursorPosition = 0;
             CursorColor = Color.Black;
             Click += OnClick;
+            MouseUp += OnMouseUp;
+            TouchUp += OnTouchUp;
             Height = 28;
             Padding = 4;
         }
 
         private void OnClick(object sender, EventArgs eventArgs)
         {
-            ScreenEngine.FocusedControl = this;
+            Focus();
+        }
+
+        private void OnMouseUp(object sender, MouseEventArgs args)
+        {
+            SetCursorFromPosition(args.Position);
+        }
+
+        private void OnTouchUp(object sender, TouchEventArgs args)
+        {
+            SetCursorFromPosition(args.Position);
         }
 
         protected internal override void OnGotFocus(GotFocusEventArgs args)
         {
             base.OnGotFocus(args);
             ScreenEngine.Instance.RequestKeyboard(InputScope);
-            if (ScreenEngine.Instance.CurrentKeyboard != null)
-                ScreenEngine.Instance.CurrentKeyboard.Control.KeyPressed += HandleKeyPressed;
+            AttachKeyboard(ScreenEngine.Instance.CurrentKeyboard);
         }
 
         protected internal override void OnLostFocus(LostFocusEventArgs args)
         {
             base.OnLostFocus(args);
-            if (ScreenEngine.Instance.CurrentKeyboard != null)
-                ScreenEngine.Instance.CurrentKeyboard.Control.KeyPressed -= HandleKeyPressed;
+            DetachKeyboard();
             ScreenEngine.Instance.HideKeyboard();
         }
 
@@ -134,23 +146,70 @@ namespace MonoGame.PortableUI.Controls
         protected internal override void OnDraw(SpriteBatch spriteBatch, Rect rect)
         {
             base.OnDraw(spriteBatch, rect);
-            rect -= Padding;
+            var textRect = rect - Padding;
+
+            if (Text.Length == 0 && !string.IsNullOrEmpty(HintText) && Font != null)
+            {
+                var measuredHint = MeasureText(HintText);
+                var offset = textRect.Offset;
+                offset.Y += (textRect.Height - measuredHint.Y) / 2;
+                if (SnapToPixel)
+                    offset = offset.ToInts();
+                spriteBatch.DrawString(Font, HintText, offset, HintTextColor);
+            }
 
             if (!IsFocused)
                 return;
 
             if (ScreenSystem.TotalTime.Milliseconds > 500)
                 return;
-            var measuredText = Font.MeasureString(Text.Substring(0, CursorPosition));
-            var x = rect.Left + measuredText.X;
-            var height = Font.MeasureString("|").Y;
-            var top = (rect.Height - height)/2 + rect.Top;
+            var measuredText = MeasureText(Text.Substring(0, CursorPosition));
+            var x = textRect.Left + measuredText.X;
+            var height = MeasureText("|").Y;
+            var top = (textRect.Height - height)/2 + textRect.Top;
             CursorColor.Draw(spriteBatch, new Rect(x, top, 1, height));
         }
 
         protected virtual void OnTextChanged(TextChangedEventArgs args)
         {
             TextChanged?.Invoke(this, args);
+        }
+
+        private void AttachKeyboard(IKeyboard keyboard)
+        {
+            if (_attachedKeyboard == keyboard)
+                return;
+            DetachKeyboard();
+            _attachedKeyboard = keyboard;
+            if (_attachedKeyboard != null)
+                _attachedKeyboard.KeyPressed += HandleKeyPressed;
+        }
+
+        private void DetachKeyboard()
+        {
+            if (_attachedKeyboard != null)
+                _attachedKeyboard.KeyPressed -= HandleKeyPressed;
+            _attachedKeyboard = null;
+        }
+
+        private void SetCursorFromPosition(PointF position)
+        {
+            var textRect = BoundingRect - Margin - Padding;
+            var x = Math.Max(0, position.X - textRect.Left);
+            var closestIndex = 0;
+            var closestDistance = float.MaxValue;
+
+            for (var i = 0; i <= Text.Length; i++)
+            {
+                var measured = MeasureText(Text.Substring(0, i)).X;
+                var distance = Math.Abs(measured - x);
+                if (distance >= closestDistance)
+                    continue;
+                closestDistance = distance;
+                closestIndex = i;
+            }
+
+            CursorPosition = closestIndex;
         }
     }
 }
